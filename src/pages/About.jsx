616b15c16c88a6616e6ddc67c82cf7f45e46c96c
Katarina Holm <katarinaholm89@gmail.com>
2026-03-13 09:44:54 +0100

    Add About.jsx and .css. Remove css from index.css

---
+++ src/pages/About.jsx
@@ -0,0 +1,54 @@
+import "./About.css"
+
+export default function About() {
+  return (
+    <>
+      <main>
+        <div id="about-me" className="container">
+          <h2>Om mig</h2>
+          <div className="about-me">
+            <figure className="grid-item">
+              <img
+                className="profile-image"
+                src="/profilbild_kvadrat.webp"
+                alt="Porträttfoto av Katarina"
+              />
+            </figure>
+            <div className="grid-item">
+              <p>
+                Jag är en vetgirig och analyserande person som gillar att förstå
+                hur saker hänger samman och att hitta smarta lösningar på
+                komplexa problem. Efter flera års arbete som logoped inom
+                hälsovård och forskning har jag förfinat mina färdigheter i att
+                lyssna, samarbeta och se helheten; kvaliteter som jag tar med
+                mig in i min nya bana som utvecklare.
+              </p>
+              <p>
+                För närvarande studerar jag till fullstackutvecklare .NET på
+                Chas Academy i Stockholm. Genom utbildningen får jag kombinera
+                mitt intresse för logiskt tänkande och problemlösning med
+                kreativitet och möjligheten att bredda min kompetens.
+              </p>
+              <p>
+                Mitt närmaste mål är att hitta en praktikplats (LIA) mellan den
+                30 november 2026 och den 18 april 2027. Jag ser fram emot att
+                bidra i ett utvecklingsteam där jag kan växa som utvecklare,
+                använda mina nya kunskaper och fördjupa mitt lärande genom
+                praktisk tillämpning.
+              </p>
+              <p>
+                Utanför studierna hittar jag energi och balans genom att träna,
+                sjunga i kör och läsa böcker. Dessa aktiviteter hjälper mig att
+                behålla fokus samt hitta inspiration och kreativitet.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Easter egg */}
+        <EasterEgg />
+      </main>
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
