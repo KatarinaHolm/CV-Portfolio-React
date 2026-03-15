d867d04fc7113b9a0337cef725392d7a084f0072
Katarina Holm <katarinaholm89@gmail.com>
2026-03-15 13:13:38 +0100

    refactored easteregg on footer to react idiomatic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import "./Footer.css"
 
 export default function Footer() {
+  const [isEggActive, setIsEggActive] = useState(false);
+
+  function handleClick()
+  {
+    setIsEggActive(isEggActive => !isEggActive);
+  }
+  
+  useEffect(() => {
+    if(isEggActive)
+    {
+      document.body.classList.add("easter-egg1");
+    }
+    else{
+      document.body.classList.remove("easter-egg1");
+    }
+   
+  }, [isEggActive])
+
+
+
   return (
-    <footer>
+    <footer onClick={handleClick}>
       <div className="center">
         <a href="https://www.linkedin.com/in/katarina-holm-14aa4a325/">
           <img
@@ -22,3 +43,4 @@ export default function Footer() {
     </footer>
   );
 }
+
