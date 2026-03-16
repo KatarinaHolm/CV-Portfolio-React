dd8a270926f2e12f2ba013551ecf4f9d9c4919a7
Katarina Holm <katarinaholm89@gmail.com>
2026-03-16 08:24:17 +0100

    Add message to visitor when loading projects from GitHub in GitHubPortfolio component

--- src/components/GitHubPortfolio.jsx
+++ src/components/GitHubPortfolio.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 // OBS! lägg till meddelande om laddar innehåll!
 export default function GitHubPortfolio() {
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   async function GetRepos() {
     const response = await fetch(
@@ -11,11 +12,22 @@ export default function GitHubPortfolio() {
     const data = await response.json();
     
     setRepos(data);
+    setLoading(false);
   }
 
   useEffect(() => {
     GetRepos();
   }, []);
+
+  if(loading){
+    return (
+      <>
+        <h2>GitHub-portfolio</h2>
+        <p>Laddar innehåll, var god vänta...</p>
+      </>
+    
+    )
+  }
 
   return (
     <>
